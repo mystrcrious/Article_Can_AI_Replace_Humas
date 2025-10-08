0ce7fdef307f7245729c1d42f6976953b6f6e52d
Rou <stephanieaurelia07@gmail.com>
2025-10-08 17:32:00 +0700

    Delete mainscript.js

--- mainscript.js
+++
@@ -1,7 +0,0 @@
-const burgerBtn = document.getElementById('burger-btn');
-  const navList = document.querySelector('nav ul');
-
-  burgerBtn.addEventListener('click', () => {
-    navList.classList.toggle('show');
-  });
-  
